08e3550eeb8d90d38364e29df445007006ab8377
Selçuk Temizsoy <selcuk@kloia.com>
2021-04-20 14:00:26 +0300

    Commit

--- src/test/java/pets/UserSimulation.scala
+++ src/test/java/pets/UserSimulation.scala
@@ -6,11 +6,11 @@ import io.gatling.core.Predef.{Simulation, atOnceUsers, openInjectionProfileFact
 import scala.concurrent.duration.DurationInt
 
 class UserSimulation extends Simulation{
-  val scenario_name = "simple"//System.getProperty("scenario")
+  val scenario_name = System.getProperty("scenario")
   val getSingleUser = scenario("simple get").exec(karateFeature("classpath:pets/"+scenario_name+".feature"))
   val postSingleUser = scenario("basic post").exec(karateFeature("classpath:pets/Postfeature.feature@name=post"))
   val protocol = karateProtocol()
-  val userCount = 10//System.getProperty("userCount")
+  val userCount = System.getProperty("userCount")
 
   setUp(
     getSingleUser.inject(rampUsers(userCount.toInt) during(10 seconds)).protocols(protocol),
